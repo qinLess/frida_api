b0fa8351cc73c1596e6cb873a4d31bd14aee8cc0
秦加虎 <qinjiahu@qinjiahudeMacBook-Pro.local>
2021-06-17 23:14:10 +0800

    update

--- frida_hook/ks/hook_data.py
+++ frida_hook/ks/hook_data.py
@@ -19,10 +19,15 @@ script_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'script.j
 logger = get_logger("ks_hook_data")
 
 apk_info = {
+    # 设备列表（存放设备列表）
     'process_list': [],
+    # app 名称
     'app_name': '快手极速版',
+    # frida hook js 文件绝对路径
     'script_path': script_path,
+    # apk 包名称
     'apk_package_name': 'com.kuaishou.nebula',
+    # apk 启动页面名称（用来自动重启 apk）
     'apk_main_activity': 'com.kuaishou.nebula/com.yxcorp.gifshow.HomeActivity'
 }
 
